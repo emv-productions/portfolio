73d46db1d2137aa6f9bfb38d97fe872163ab47cd
jmgross223694 <jmgross76@gmail.com>
2025-02-22 12:33:39 -0300

    Test githubPages

--- JS/collections.js
+++
@@ -1,6 +0,0 @@
-fetch('collections_content.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('collections_content').innerHTML = data;
-    })
-    .catch(error => console.error('Error cargando el archivo:', error));
